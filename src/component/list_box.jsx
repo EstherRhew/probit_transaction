6b16ddb040deab9700c96a23a4ecb27f73c3a78a
Yoonjung Rhew <koderhew@gmail.com>
2021-11-25 18:38:34 +0900

    made filtered function on platform tab click

--- src/component/list_box.jsx
+++ src/component/list_box.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ListItem from './list_item';
 
 const ListBox = ({ list, dateWithZero, getTickerTime, deleteList }) => {
+  const [tab, setTab] = useState('all')
+
+  const onClickTab = (e) => {
+    console.log(e.target.textContent.toLowerCase())
+    setTab(e.target.textContent.toLowerCase())
+  }
+
+  const filterList = (list) => {
+    if (tab === 'all') {
+      return list;
+    }
+    const filteredList = list.filter((item) => item.platform === tab)
+    return filteredList;
+  }
+
   return (
     <div className="list_box">
-      <ul className="tabs">
-        <li className="tab">All</li>
+      <ul className="tabs" onClick={onClickTab}>
+        <li className="tab clicked">All</li>
         <li className="tab">Probit</li>
         <li className="tab">Bittrex</li>
         <li className="tab">Bibox</li>
@@ -20,7 +35,7 @@ const ListBox = ({ list, dateWithZero, getTickerTime, deleteList }) => {
           <span className="cell">설정시간</span>
           <span className="cell">타이머</span>
         </li>
-        {list && list.map((item, index) =>
+        {list && filterList(list).map((item, index) =>
           <ListItem item={item} key={item.id} number={index + 1} dateWithZero={dateWithZero} getTickerTime={getTickerTime} deleteList={deleteList} />
         )}
       </ul>
